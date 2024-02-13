04629f83935f967011f5d636ae62f3f8bf4f40bf
Laura Crawford <laura.crawford@thinkmoney.co.uk>
2024-02-13 13:36:46 +0000

    %T to find data type of variables

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50 
+	var remainingTickets uint = 50 
+
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
 	fmt.Printf("Only %v tickets left out of %v spots\n", remainingTickets, conferenceTickets )
