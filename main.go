ce8d6ee61783fdf05d6579c41bf6db5f01d8122b
Laura Crawford <laura.crawford@thinkmoney.co.uk>
2024-02-13 15:50:21 +0000

    if statement to break loop if 50 tickets booked up

--- main.go
+++ main.go
@@ -49,5 +49,11 @@ func main() {
 			 firstNames = append(firstNames, names[0])
 		}
 		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+
+		if remainingTickets == 0 {
+			fmt.Println("Our conference is booked out. Come back next year")
+			break
+		}
 	}
 }
