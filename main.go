2bb951ea69de67a568af2e63645ef50433d6ab27
Laura Crawford <laura.crawford@thinkmoney.co.uk>
2024-02-22 16:03:15 +0000

    order bookings shows message if validation not met

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"gopractice-bookingapp/helper"
 	"log"
 	"net/http"
 
@@ -29,48 +30,21 @@ import (
 		NumberOfTickets uint `json:"numberOfTickets"`
 	}
 
-	// var wg = sync.WaitGroup{}
+	type BookingError struct {
+		Message string `json:"message"`
+	}
+
 
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/conference", getConferenceDetails).Methods("GET")
 	r.HandleFunc("/bookings", getBookings).Methods("GET")
-	// r.HandleFunc("/order", handleOptions)
 	r.HandleFunc("/order", postTicketsOrder).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8000", r))
-
-	
-		// firstName, lastName, email, userTickets := getUserInput()
-		// isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
-
-		// if isValidName && isValidEmail && isValidTicketNumber{
-		// 	bookings, remainingTickets = bookTickets(remainingTickets, userTickets, firstName, lastName, email, orderId)
-		// 	wg.Add(1)
-		// 	// go sendTicket(userTickets, firstName, lastName, email)
-
-
-		// if remainingTickets == 0 {
-		// 	fmt.Println("Our conference is booked out. Come back next year")
-		// }
-		// } else {
-		// 	if !isValidName{
-		// 		fmt.Print("First name or last name you entered is too short\n")
-		// 	}
-		// 	if !isValidEmail{
-		// 		fmt.Print("Email address you entered does not contain an @ sign\n")
-		// 	}
-		// 	if !isValidTicketNumber{
-		// 		fmt.Print("The number of tickets you've entered is invalid\n")
-		// 	}
-		// }
-		// wg.Wait()
+	log.Fatal(http.ListenAndServe(":8000", r))		
 	}
 
-
-
-func bookTickets(remainingTickets uint, userTickets uint, firstName string, lastName string, email string, orderID string) ([]UserData, uint){
-	remainingTickets = remainingTickets - userTickets
+func bookTickets(remainingTickets uint, userTickets uint, firstName string, lastName string, email string, orderID string) ([]UserData, uint, string){
 
 	var userData = UserData {
 		OrderID: orderID,
@@ -79,9 +53,70 @@ func bookTickets(remainingTickets uint, userTickets uint, firstName string, last
 		Email: email,
 		NumberOfTickets: userTickets,
 	}
+	
+isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
-	bookings = append(bookings, userData)
-return bookings, remainingTickets
+var bookingErrorMessage string
+
+    if !isValidName {
+        bookingErrorMessage += "First name or last name you entered is too short"
+    }
+    if !isValidEmail {
+        bookingErrorMessage += "Email address you entered does not contain an @ sign"
+    }
+    if !isValidTicketNumber {
+        bookingErrorMessage += "The number of tickets you've entered is invalid"
+    }
+
+    if !isValidName || !isValidEmail || !isValidTicketNumber {
+        return nil, remainingTickets, bookingErrorMessage
+    }
+	
+	 bookings = append(bookings, userData)
+
+	remainingTickets = remainingTickets - userTickets
+return bookings, remainingTickets, bookingErrorMessage
+}
+
+func postTicketsOrder(w http.ResponseWriter, r *http.Request){
+	fmt.Println("request method:", r.Method)
+	(w).Header().Set("Content-Type", "application/json")
+	 w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	var order UserData
+	var bookingErrorMessage string 
+	err := json.NewDecoder(r.Body).Decode(&order)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	orderID:= uuid.New().String()
+	order.OrderID = orderID
+	bookings, remainingTickets, bookingErrorMessage = bookTickets(remainingTickets, order.NumberOfTickets, order.FirstName, order.LastName, order.Email, order.OrderID)
+	
+	 var response interface{}
+
+    if bookingErrorMessage != "" {
+        response = struct {
+            Error *BookingError `json:"error"`
+        }{
+            Error: &BookingError{Message: bookingErrorMessage},
+        }
+    } else {
+        response = struct {
+            UserData        UserData `json:"order"`
+            RemainingTickets uint     `json:"remainingTickets"`
+        }{
+            UserData:        order,
+            RemainingTickets: remainingTickets,
+        }
+    }
+
+	
+	json.NewEncoder(w).Encode(response)
 }
 
 func getConferenceDetails( w http.ResponseWriter, r *http.Request) {
@@ -100,72 +135,3 @@ func getBookings( w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(bookings)
 }
-
-// func handleOptions(w http.ResponseWriter, r *http.Request) {
-//    w.Header().Set("Access-Control-Allow-Origin", "*")
-//     w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-//     w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-//  w.WriteHeader(http.StatusOK)
-// }
-
-
-func postTicketsOrder(w http.ResponseWriter, r *http.Request){
-	fmt.Println("request method:", r)
-	   
-
-    // Respond with OK status
-    w.WriteHeader(http.StatusOK)
-	(w).Header().Set("Content-Type", "application/json")
-	 w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	var order UserData
-	err := json.NewDecoder(r.Body).Decode(&order)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-
-   
-fmt.Println(w, "order: %+v", order)
-
-
-	orderID:= uuid.New().String()
-	order.OrderID = orderID
-	bookings, remainingTickets = bookTickets(remainingTickets, order.NumberOfTickets, order.FirstName, order.LastName, order.Email, order.OrderID)
-	json.NewEncoder(w).Encode(bookings)
-}
-
-// func getUserInput()(string, string, string, uint){
-// 	var firstName string
-// 		var lastName string
-// 		var email string
-// 		var userTickets uint
-
-// 		fmt.Println("Enter your first name: ")
-// 		fmt.Scan(&firstName)
-
-// 		fmt.Println("Enter your last name: ")
-// 		fmt.Scan(&lastName)
-
-// 		fmt.Println("Enter your email address: ")
-// 		fmt.Scan(&email)
-
-// 		fmt.Println("Enter amount of tickets: ")
-// 		fmt.Scan(&userTickets)
-
-// 		return firstName, lastName, email, userTickets
-// }
-
-// func sendTicket(userTickets uint, firstName string, lastName string, email string){
-// 	time.Sleep(10 * time.Second)
-// 	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
-// 	fmt.Println("#################")
-// 	fmt.Printf("Sending tickets:\n %v \nto email address %v", ticket, email)
-// 	fmt.Println("#################")
-// 	wg.Done()
-// }
-
